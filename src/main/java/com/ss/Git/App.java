a68864f55d74551b2be736feb0302487dff9ed0b
ss <ss@163.com>
2022-09-14 20:32:40 +0800

    master commit version

--- src/main/java/com/ss/Git/App.java
+++ src/main/java/com/ss/Git/App.java
@@ -14,5 +14,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! master refresh" );
     }
 }
