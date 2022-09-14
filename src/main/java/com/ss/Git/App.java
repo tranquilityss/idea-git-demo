be7931b26852029a6cf1045b9cba228c2bf81328
ss <ss@163.com>
2022-09-14 20:27:47 +0800

    one version

---
+++ src/main/java/com/ss/Git/App.java
@@ -0,0 +1,18 @@
+package com.ss.Git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+    }
+}
